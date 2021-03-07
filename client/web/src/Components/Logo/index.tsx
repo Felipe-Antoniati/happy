ece828130e9c7a43aa607d693494bb1335b4708f
felipe-antoniati <antoniati.felipe@gmail.com>
2021-03-07 09:25:32 -0300

    page Map: add Map, modify header, add Buttons

--- client/web/src/Components/Logo/index.tsx
+++ client/web/src/Components/Logo/index.tsx
@@ -1,10 +1,10 @@
-import markerImg from "../../assets/marker.png";
+import mapMarkerImg from "../../assets/marker.svg";
 import "./styles.css";
 
 export default function Logo() {
   return (
     <div className="logo">
-      <img src={markerImg} alt="Logo" />
+      <img src={mapMarkerImg} alt="Logo" />
       <h1>happy</h1>
     </div>
   );
--- client/web/src/pages/Map/index.tsx
+++ client/web/src/pages/Map/index.tsx
@@ -1,6 +1,23 @@
 import Logo from "../../Components/Logo";
-import { FiInfo, FiArrowLeftCircle } from "react-icons/fi";
 import { Link } from "react-router-dom";
+
+import {
+  FiInfo,
+  FiPlus,
+  FiArrowRight,
+  FiGithub,
+  FiLinkedin
+} from "react-icons/fi";
+
+import { 
+  MapContainer, 
+  TileLayer, 
+  Marker, 
+  Popup 
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import mapIcon from "../../utils/mapIcon";
+
 import "./styles.css";
 
 export default function Map() {
@@ -8,22 +25,55 @@ export default function Map() {
     <div id="map-page">
       <div className="map-content">
         <header>
-          <div>
+        <div>
             <Link to="/">
-              <FiArrowLeftCircle />
+              <FiLinkedin />
             </Link>
             <Link to="/map">
-              <FiInfo />
+              <FiGithub />
             </Link>
+            <Link to="/map">
+              <FiInfo size={30}/>
+            </Link>            
           </div>
           <h2>
-            Escolha um ponto no mapa para poder
-            <strong> visitar</strong>
+            <b>Bertioga,</b>
+            <i> São Paulo</i>
           </h2>
           <Logo />
         </header>
         <main>
-          Aqui vai um mapa
+          <MapContainer
+            center={[-23.8413458, -46.1422318]}
+            zoom={15}
+            style={{ 
+              width: "100%", 
+              height: "100%" 
+            }}
+          >
+            <TileLayer 
+              url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+            />
+            <Marker 
+              icon={mapIcon} 
+              position={[-23.8413458, -46.1422318]}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                <h1>Instituto Casa Lar - Minas Gerais</h1>
+                <Link to="/map" className="go-hs">
+                  <FiArrowRight />
+                </Link>
+              </Popup>
+            </Marker>
+          </MapContainer>
+          <Link to="/map" className="add-hs">
+            <FiPlus />
+          </Link>
         </main>
       </div>
     </div>
